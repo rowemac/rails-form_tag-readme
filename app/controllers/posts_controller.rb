cab6cae6e20db642c1ae3e32d1615b58de46dc6b
Mac Rowe <m.rowe42@gmail.com>
2021-04-21 12:39:10 -0400

    commit tree 2c92b353854d71963b26e6055ac3e444b46f5d4e

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,13 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def create
+    Post.create(title: params[:post][:title], description: params[:post][:description])
+    redirect_to posts_path
+  end
   
+  def new
+  end 
+
 end
